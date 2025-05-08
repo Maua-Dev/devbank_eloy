a9248379ea47a2b30c7b2987f1f3355a41c5febe
veloy25 <veloy.devmaua@gmail.com>
2025-05-07 22:16:05 -0300

    item.py validation done

--- src/app/entities/item.py
+++ src/app/entities/item.py
@@ -5,91 +5,79 @@ from ..enums.item_type_enum import ItemTypeEnum
 
 class Item:
     name: str
-    account: float
+    account: str
     agency: str
     current_balance: float
     
     def __init__(self, name: str=None,  account : float=None, agency: str=None, current_balance: float = None):
-    #    validation_name = self.validate_name(name)
-    #    if validation_name[0] is False:
-    #        raise ParamNotValidated("name", validation_name[1])
-    #    self.name = name
-    #    
-    #    validation_price = self.validate_price(price)
-    #    if validation_price[0] is False:
-    #        raise ParamNotValidated("price", validation_price[1])
-    #    self.price = price
-    #
-    #    validation_item_type = self.validate_item_type(item_type)
-    #    if validation_item_type[0] is False:
-    #        raise ParamNotValidated("item_type", validation_item_type[1])
-    #    self.item_type = item_type
-    #    
-    #    validation_admin_permission = self.validate_admin_permission(admin_permission)
-    #    if validation_admin_permission[0] is False:
-    #        raise ParamNotValidated("admin_permission", validation_admin_permission[1])
-    #    self.admin_permission = admin_permission
-    #    
-    #@staticmethod
-    #def validate_name(name: str) -> Tuple[bool, str]:
-    #    if name is None:
-    #        return (False, "Name is required")
-    #    if type(name) != str:
-    #        return (False, "Name must be a string")
-    #    if len(name) < 3:
-    #        return (False, "Name must be at least 3 characters long")
-    #    return (True, "")
-    #    
-    #@staticmethod
-    #def validate_price(price: float) -> Tuple[bool, str]:
-    #    if price is None:
-    #        return (False, "Price is required")
-    #    if type(price) != float:
-    #        return (False, "Price must be a float")
-    #    if price < 0:
-    #        return (False, "Price must be a positive number")
-    #    return (True, "")
-    #
-    #@staticmethod
-    #def validate_item_type(item_type: ItemTypeEnum) -> Tuple[bool, str]:
-    #    if item_type is None:
-    #        return (False, "Item type is required")
-    #    if type(item_type) != ItemTypeEnum:
-    #        return (False, "Item type must be a ItemTypeEnum")
-    #    return (True, "")
-    #
-    #@staticmethod
-    #def validate_admin_permission(admin_permission: bool) -> Tuple[bool, str]:
-    #    if admin_permission is None:
-    #        return (False, "Admin permission is required")
-    #    if type(admin_permission) != bool:
-    #        return (False, "Admin permission must be a boolean")
-    #    return (True, "")
-    #    
-    #@staticmethod
-    #def validate_item_id(item_id: int) -> Tuple[bool, str]:
-    #    if item_id is None:
-    #        return (False, "Missing 'item_id' parameter")
-    #
-    #    if type(item_id) != int:
-    #        return (False, "Parameter 'item_id' must be an integer")
-    #    
-    #    if item_id < 0:
-    #        return (False, "Parameter 'item_id' must be a positive integer")
-    #
-    #    return (True, "")
-    #
-    #    
-    #def to_dict(self):
-    #    return {
-    #        "name": self.name,
-    #        "price": self.price,
-    #        "item_type": self.item_type.value,
-    #        "admin_permission": self.admin_permission
-    #    }
-    #
-    #def __eq__(self,other):
-    #    return self.name == other.name and self.price == other.price and self.item_type == other.item_type and self.admin_permission == other.admin_permission
-    #
-    #def __repr__(self):
-    #    return f"Item(name={self.name}, price={self.price}, item_type={self.item_type}, admin_permission={self.admin_permission})"
+        
+        validation_name = self.validate_name(name)
+        if validation_name[0] is False:
+            raise ParamNotValidated("name", validation_name[1])
+        self.name = name
+
+        validation_account = self.validate_account(account)
+        if validation_account[0] is False:
+            raise ParamNotValidated("account", validation_name[1])
+        self.account = account
+        
+        validation_agency = self.validate_agency(agency)
+        if validation_agency[0] is False:
+            raise ParamNotValidated("agency", validation_agency[1])
+        self.agency = agency
+        
+        validation_current_balance = self.validate_current_balance(current_balance)
+        if validation_current_balance[0] is False:
+            raise ParamNotValidated("current balance", validation_agency[1])
+        self.current_balance = current_balance
+        
+        
+    
+    @staticmethod
+    def validate_name(name: str) -> Tuple[bool, str]:
+        if name is None:
+            return (False, "Name is required")
+        if type(name) != str:
+            return (False, "Name must be a string")
+        if len(name) < 3:
+            return (False, "Name must be at least 3 characters long")
+        return (True, "")
+
+    @staticmethod
+    def validate_account(account: float) -> Tuple[bool, str]:
+        if account is None:
+            return(False, "Account is required")
+        if type(account) != str:
+            return(False, "Account must be a string")
+        if len(account) != 4:
+            return(False, "Account must have 4 numbers")
+        return (True, "")
+    
+    @staticmethod
+    def validate_agency(agency: str) -> Tuple [bool, str]:
+        if agency is None:
+            return(False, "Account is required")
+        if type(agency)!= str:
+            return(False, "Agency must be a string")
+        if len(agency)!= 6:
+            return (False, "Agency must have 6 characters")
+    
+    
+    @staticmethod
+    def validate_current_balance(current_balance: float) -> Tuple[bool, str]:
+        if current_balance is None:
+            return(False, "Current Balance is required")
+        if type(current_balance) != float:
+            return(False, "Current Balance must be a float")
+        if current_balance < 0:
+            return(False, "Current Balance must be 0 or a positive number")
+    
+    def to_dict(self):
+       return {
+           "name": self.name,
+           "account": self.account,
+           "agency": self.agency,
+           "current_balance": self.current_balance
+       }
+    
+    
